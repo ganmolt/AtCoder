dcd1bf0a1db7a0c78c5ad980e08086ec4b80c2c9
ganmo <amano_youta@yahoo.co.jp>
2022-07-19 15:07:08 +0900

    :fire:

---
+++ ABC/abc051-100/abc058/c.cpp
@@ -0,0 +1,49 @@
+//INCLUDE
+//------------------------------------------
+#include <bits/stdc++.h>
+//DEFINE
+//------------------------------------------
+typedef long long int Int;
+typedef long double Double;
+#define dcml(n) fixed<<setprecision(n)
+#define YES cout<<"Yes\n",exit(0)
+#define NO cout<<"No\n",exit(0)
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+//CONST
+//------------------------------------------
+const Int INF=1010000000000000017LL;
+const Int MOD=1000000007LL;
+const Double EPS=1e-12;
+const Double PI=3.14159265358979323846;
+//REPEAT
+//------------------------------------------
+#define   REP(i,n)   for(Int (i)=0;     (i)<(n);  (i)++)
+#define   FOR(i,k,n)   for(Int (i)=(k);     (i)<(n);  (i)++)
+//-----------------------------------------
+//namespace
+using namespace std;
+//ライブラリはここに
+//↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+//-----------------------------------------
+int main(void){
+    int n;cin>>n;
+    vector<string> s(n);
+    REP(i,n)cin>>s[i];
+
+    vector<map<char,int>> vm(n);
+    REP(i,n){
+        for(auto r:s[i]){
+            vm[i][r]++;
+        }
+    }
+    string ans="";
+    REP(i,26){
+        int cnt=1e9;
+        REP(j,n){
+            chmin(cnt,vm[j]['a'+i]);
+        }
+        REP(k,cnt)ans.push_back(char(i+'a'));
+    }
+    cout<<ans<<"\n";
+}
