bbaa552f7eff0d40bf8e1d7d83d8e2e785f24e83
ganmo <amano_youta@yahoo.co.jp>
2022-08-10 23:58:13 +0900

    :fire:

---
+++ ABC/abc251-300/abc263/c.cpp
@@ -0,0 +1,51 @@
+//INCLUDE
+//------------------------------------------
+#include <bits/stdc++.h>
+//DEFINE
+//------------------------------------------
+typedef long long int Int;
+typedef long double Double;
+#define dcml(n) fixed<<setprecision(n)
+#define YES cout<<"Yes\n",exit(0)
+#define NO cout<<"No\n",exit(0)
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return 1; } return 0; }
+//CONST
+//------------------------------------------
+const Int INF=1010000000000000017LL;
+const Int MOD=1000000007LL;
+const Double EPS=1e-12;
+const Double PI=3.14159265358979323846;
+//REPEAT
+//------------------------------------------
+#define   REP(i,n)   for(Int (i)=0;     (i)<(n);  (i)++)
+#define   FOR(i,k,n)   for(Int (i)=(k);     (i)<(n);  (i)++)
+//-----------------------------------------
+//namespace
+using namespace std;
+//ライブラリはここに
+//↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+//-----------------------------------------
+int n, m;
+vector<vector<int>> v;
+void dfs(int i, vector<int> _v){
+    if(i > m)return;
+    if(_v.size() == n){
+        v.push_back(_v);
+        return;
+    }
+    dfs(i+1, _v);
+    _v.push_back(i+1);
+    dfs(i+1, _v);
+}
+int main(void){
+    cin >> n >> m;
+    dfs(0, {});
+
+    sort(v.begin(),v.end());
+    for(auto r:v){
+        for(auto c:r){
+            cout<<c<<" ";
+        }cout<<"\n";
+    }
+}
